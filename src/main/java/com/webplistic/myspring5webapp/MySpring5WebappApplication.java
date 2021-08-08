90bb0f1d207e9314cd7bbf463c4abf7d0f218ce5
Tony Marcum <marcuma@gmail.com>
2021-08-08 15:31:01 -0400

    Adding project. Book and Author pojo already existed.

---
+++ src/main/java/com/webplistic/myspring5webapp/MySpring5WebappApplication.java
@@ -0,0 +1,13 @@
+package com.webplistic.myspring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpring5WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpring5WebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webplistic/myspring5webapp/MySpring5WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webplistic.myspring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpring5WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
